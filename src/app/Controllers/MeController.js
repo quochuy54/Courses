eb8443695e98d3c145bf4cfacdeb207d7da8d281
quochuy54 <lunladoithu284@gmail.com>
2022-08-22 14:39:26 +0700

    Update-feature-sort-strore/trash-courses

--- src/app/Controllers/MeController.js
+++ src/app/Controllers/MeController.js
@@ -4,12 +4,12 @@ class MeController{
 
     //[GET] /me/stored/courses
     storedCourses(req, res, next){
-        let courseQuery = Course.find({})
-        // if(req.query.hasOwnProperty('_sort')){
-        //     courseQuery = courseQuery.sort({
-        //         [req.query.column]: req.query.type
-        //     })
-        // }
+        let courseQuery = Course.find({});
+        if(req.query.hasOwnProperty('_sort')){
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+        }
 
         Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses, deleteCount]) => {
@@ -22,8 +22,15 @@ class MeController{
 
     //[GET] /me/trash/courses
     trashCourses(req, res, next){
-        Course.findDeleted({})
-            .then(courses => res.render('me/trash-courses', {courses: MutiMongooseToOjbect(courses)}))
+        let courseDeleted = Course.findDeleted({});
+
+        if(req.query.hasOwnProperty('_sort')){
+            courseDeleted = courseDeleted.sort({
+                [req.query.column]: req.query.type
+            })
+        }
+          
+        courseDeleted.then(courses => res.render('me/trash-courses', {courses: MutiMongooseToOjbect(courses)}))
             .catch(next)
     }
 
--- src/app/Middlewares/SortMiddleware.js
+++ src/app/Middlewares/SortMiddleware.js
@@ -1,7 +1,7 @@
 module.exports = function SortMiddleware(req, res, next) {
 
     res.locals._sort = {
-        enabled: false,
+        // enabled: false,
         type: 'default'
     }
 
@@ -11,7 +11,7 @@ module.exports = function SortMiddleware(req, res, next) {
         // res.locals._sort.column = req.query.column
 
         Object.assign(res.locals._sort, {
-            enabled: true,
+            // enabled: true,
             type: req.query.type,
             column: req.query.column
         })
